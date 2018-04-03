6ae9ce7dda393d196d35df78749670bca6e6be85
sheldonwoodward <sheldon.k.woodward@gmail.com>
2018-04-02 19:04:37 -0700

    Added docstrings.

--- dict_tree/DictTree.py
+++ dict_tree/DictTree.py
@@ -1,15 +1,26 @@
 # sheldon woodward
 # 3/29/18
 
+"""DictTree Class"""
+
 
 class DictTree:
     def __init__(self, depth=0):
-        """A search tree """
+        """A search tree for verifying words are valid.
+
+        depth (int):
+            The depth of the tree. Typically not altered and used internally.
+        """
         self.characters = {}
         self.depth = depth
         self.valid = False
 
     def add_word(self, word):
+        """Add a word to the DictTree dictionary.
+
+        word (str):
+            The word to be added.
+        """
         if self.depth < len(word):
             if word[self.depth] not in self.characters:
                 self.characters[word[self.depth]] = DictTree(self.depth + 1)
@@ -18,6 +29,11 @@ class DictTree:
             self.valid = True
 
     def is_word(self, word):
+        """Verify a word exists in the dictionary.
+
+        word (str):
+            The word to be verified.
+        """
         if self.depth < len(word):
             if word[self.depth] not in self.characters:
                 return False
